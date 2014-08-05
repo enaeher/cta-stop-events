41a826d86de2041e29174f8843e17f04a0c9cd5b
Eli Naeher <enaeher@gmail.com>
2014-08-05 09:41:04 -0500

    Change where we do our integer parsing in xpath->simple-date, to appease SBCL's type inference. (See http://thread.gmane.org/gmane.lisp.steel-bank.devel/13265/focus=13266)

--- bus-api/util.lisp
+++ bus-api/util.lisp
@@ -25,10 +25,26 @@
   (xpath:string-value (xpath:evaluate xpath node)))
 
 (defun xpath->simple-date (node xpath)
-  (ppcre:register-groups-bind (year month day hour minute)
+  (ppcre:register-groups-bind (('parse-integer year)
+                               ('parse-integer month)
+                               ('parse-integer day)
+                               ('parse-integer hour)
+                               ('parse-integer minute))
       ("(\\d{4})(\\d{2})(\\d{2}) (\\d{2}):(\\d{2})" (xpath->string node xpath) :sharedp t)
-    (simple-date:encode-timestamp (parse-integer year)
-                                  (parse-integer month)
-                                  (parse-integer day)
-                                  (parse-integer hour)
-                                  (parse-integer minute))))
+    (simple-date:encode-timestamp year month day hour minute)))
+
+;; GROUP is borrowed from Paul Graham's On Lisp by way of
+;; http://lisp-univ-etc.blogspot.com/2013/01/real-list-comprehensions-in-lisp.html
+
+(defun group (n list)
+  "Split LIST into a list of lists of length N."
+  (declare (integer n))
+  (when (zerop n)
+    (error "Group length N shouldn't be zero."))
+  (labels ((rec (src acc)
+             (let ((rest (nthcdr n src)))
+               (if (consp rest)
+                   (rec rest (cons (subseq src 0 n) acc))
+                   (nreverse (cons src acc))))))
+    (when list
+            (rec list nil))))
