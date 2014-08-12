0cffaafe2b48c76e28eb348aaeb90aec805a598a
Eli Naeher <enaeher@gmail.com>
2014-08-12 14:08:15 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sbcl --disable-debugger <<EOF
-  (asdf:initialize-source-registry "")
+  (asdf:initialize-source-registry (sb-posix:getcwd))
   (ql:quickload 'cta)
   (sb-ext:save-lisp-and-die "chicago-transit" :executable t)
 EOF
